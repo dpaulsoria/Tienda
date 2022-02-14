561576bf912f4f409708cfb3b464a61684fa69b6
Paul Soria <dpaulsoria@gmail.com>
2022-02-13 23:55:03 -0500

    PARSEINT ID

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -16,19 +16,33 @@ router.get('/all', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-  const { id } = req.params
+  const id = parseInt(req.params.id)
+
+  console.log("GET " + id)
+
   if (id < 0) res.status(404).json({ message: 'Not found' })
-  else res.json(service.findById(id))
+  else res.status(200).json(service.find(id))
 })
 
 router.delete('/:id', (req, res) => {
-  const { id } = req.params
+  const id = parseInt(req.params.id)
 
-  if (id < 0) res.send('No book')
-  else {
-    service.books[id] = {}
-    res.status(200).json({ deleted: true })
-  }
+  console.log("DELETE " + id)
+
+  if (id < 0) res.status(404).json({ message: 'Book not found to delete' })
+  else res.status(200).json(service.delete(id))
+
+})
+
+router.patch('/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const body = req.body ;
+
+  console.log("PATCH " + id)
+  console.log(body)
+
+  if (id < 0) res.status(404).json({ message: 'Book not found to update' })
+  else res.status(200).json(service.update(id, body))
 })
 
 module.exports = router
--- services/booksService.js
+++ services/booksService.js
@@ -5,31 +5,38 @@ class BooksService {
 
   }
   create(data) {
+    const newId = this.books.length
     const newBook = {
-      "id": this.books.length,
+      "id": newId,
       ...data
     }
     this.books.push(newBook)
+    return { newId }
   }
 
-  findById(id) {
-    return this.books[id]
+  find(id) {
+    const index = this.books.findIndex(item => item.isbn === id || item.id === id)
+    if (index === -1) throw new Error('Book not found')
+    else return this.books[index]
   }
 
-  find() {
-    return this.books
+  update(id, changes) {
+    const index = this.books.findIndex(item => item.isbn === id || item.id === id)
+    const newBook = {
+      "id": parseInt(id, 10),
+      ...changes
+    }
+    console.log(index, newBook, changes)
+    if (index === -1) throw new Error('Book not found')
+    else this.books[index] = newBook
+    return { message: "updated", "id": id }
   }
 
-  findOne() {
-
-  }
-
-  update() {
-
-  }
-
-  delete() {
-
+  delete(id) {
+    const index = this.books.findIndex(item => item.isbn === id || item.id === id)
+    if (index === -1) throw new Error('Book not found')
+    else this.books.splice(index, 1)
+    return { message: "deleted", "id": id }
   }
 }
 
